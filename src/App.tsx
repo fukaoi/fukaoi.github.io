3735366bf04a1e6d3ea4218a8e138c8c8bf6bf40
fukaoi <goodakira7@gmail.com>
2020-07-14 22:16:09 +0900

    Fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Create stellar keypair</h2>
       </header>
+      <div className="container">
+        <h2>Generate Hex 64byte</h2>
+        <button>Generate</button>
+        <br />
+        <br />
+        <br />
+        <h2>Generate Stellar Keypair</h2>
+        <input size={60} maxLength={64}  type="text" name="hex" placeholder="e.g: Hex 64byte string" />
+        <br />
+        <br />
+        <button>Generate</button>
+      </div>
     </div>
   );
 }
