f5e42db505987fa7c15544b59d4e7acee318ab0c
fukaoi <goodakira7@gmail.com>
2020-07-17 22:08:13 +0900

    Modified ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,12 @@ import {generateKeypair} from './generateKeypair';
 import './App.css';
 
 function App() {
-  const [hex, setHex] = useState('');
-  const [value, setValue] = useState('');
+  const [seed, setSeed] = useState('');
   const [keypair, setKeypair] = useState({pubkey: '', secret: ''});
-  const handleHexGen = () => {
-    const hex = cryptoRandomString({length: 64});
-    setHex(hex);
-    setValue(hex);
-  }
-
-  const handleInputType = (event: any) => 
-    setValue(event.target.value);
-
+  const handleHexGen = () => setSeed(cryptoRandomString({length: 64}));
+  const handleInputType = (event: any) => setSeed(event.target.value);
   const handleKeypairGen = (seed: string) => {
-    setValue(seed);
+    setSeed(seed);
     setKeypair(generateKeypair(seed));
   }
 
@@ -29,21 +21,17 @@ function App() {
       <div className="container">
         <h2>Generate Hex 64byte</h2>
         <button onClick={handleHexGen}>Generate</button>
-        <p>Hex(64byte): {hex}</p>
-
         <div className="space" />
-
         <h2>Generate Stellar Keypair</h2>
         <input
           size={60}
           maxLength={64}
           type="text"
-          name="hex"
-          value={value}
-          placeholder="e.g: Hex 64byte string"
+          value={seed}
+          placeholder="e.g: Seed(Hex 64byte string)"
           onChange={handleInputType}
         />
-        <button onClick={() => handleKeypairGen(value)}>Generate</button>
+        <button onClick={() => handleKeypairGen(seed)}>Generate</button>
         <p>pubkey: {keypair.pubkey}</p>
         <p>secret: {keypair.secret}</p>
 
