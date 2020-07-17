a780eec8eea395b175f1d589bce23afb144ba2e6
fukaoi <goodakira7@gmail.com>
2020-07-17 22:28:56 +0900

    Updates

---
+++ precache-manifest.0e347aa32833c454b8a2952880c44319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d620b8f7cd5989a5b24649f2329b7bc",
+    "url": "/fukaoi.github.io/index.html"
+  },
+  {
+    "revision": "65bbe0870c85e303f4ae",
+    "url": "/fukaoi.github.io/static/css/main.076cff46.chunk.css"
+  },
+  {
+    "revision": "dbc8e87e0575c46c1b4a",
+    "url": "/fukaoi.github.io/static/js/2.157723ec.chunk.js"
+  },
+  {
+    "revision": "1e1e8f3da23d8a122d2b083e6f7bdd83",
+    "url": "/fukaoi.github.io/static/js/2.157723ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65bbe0870c85e303f4ae",
+    "url": "/fukaoi.github.io/static/js/main.df356e18.chunk.js"
+  },
+  {
+    "revision": "5816ef261f724a76541f",
+    "url": "/fukaoi.github.io/static/js/runtime-main.072bc278.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1953afee9da4ed101e256da4d24dcad3.js"
+  "/fukaoi.github.io/precache-manifest.0e347aa32833c454b8a2952880c44319.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fukaoi.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
